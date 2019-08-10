8c53af7ef54967667ae63995db6721d81abfd3df
Sergio Ibarra Espinosa <sergio.ibarra@usp.br>
2019-08-10 16:53:16 -0300

    xtractor now supports many points
     or 2manypoints  (http://2manydjs.com/)

--- R/xtractor.R
+++ R/xtractor.R
@@ -26,9 +26,9 @@
 #' #use your wrfout
 #' #wrf <- "~/Documentos/wrfo/wrfoA.nc"
 #' t2 = c("T2", "o3", "no")
-#' df <- xtractor(atmos = wrf, vars = t2, points = cetesb[1:3, ],
+#' df <- xtractor(atmos = wrf, vars = t2, points = cetesb,
 #' stations = cetesb$Station[1:3])
-#' r <- xtractor(atmos = wrf, vars = t2, points = cetesb[1, ], return_list = T)
+#' r <- xtractor(atmos = wrf, vars = t2, points = cetesb return_list = T)
 #' }
 xtractor <- function (atmos,
                        vars,
@@ -39,22 +39,23 @@ xtractor <- function (atmos,
                        model = "WRF",
                        tz = "America/Sao_Paulo",
                        return_list = FALSE,
-                       verbose = TRUE)
-{
+                       verbose = TRUE){
+# desde aqui
   if (class(atmos)[1] == "character") {
-    if (nrow(points) > 1) {
-      stop("Currently supports points with one row. Use 'for' or 'lapply'.")
-    }
-
+	  
     if(verbose) cat(paste0("Reading NetCDF \n"))
+    
     xx <- ncdf4::nc_open(atmos)
     lat <- ncdf4::ncvar_get(xx, "XLAT")
     lon <- ncdf4::ncvar_get(xx, "XLONG")
     times <- ncdf4::ncvar_get(xx, "Times")
     times <- gsub(pattern = " ", replacement = "_", x = times)
     times <- paste0("H", times)
+    
     lr <- list()
+    
     if(verbose) cat(paste0("Converting to raster \n"))
+    
     for (i in 1:length(vars)) {
       x <- eixport::wrf_get(file = atmos, name = vars[i],
                             as_raster = FALSE)
@@ -81,8 +82,10 @@ xtractor <- function (atmos,
     }
     names(lr) <- vars
   }
+# hasta aqui
 
-
+# desde aqui
+    if(verbose) cat(paste0("Points to sp\n"))
   if (class(points)[1] == "matrix" | class(points)[1] == "data.frame") {
     points <- as.data.frame(points)
     names(points) <- c("x", "y", "Station")
@@ -91,35 +94,33 @@ xtractor <- function (atmos,
                            crs = crs_points)
     if (crs_points != 4326)
       points <- sf::st_transform(points, 4326)
-    points <- as(points, "Spatial")
-  }
-  else if (class(points)[1] == "sf") {
+  } else if (class(points)[1] == "sf") {
     names(points) <- c("Station", "geometry")
     if (crs_points != 4326)
       points <- sf::st_transform(points, 4326)
-    points <- as(points, "Spatial")
-  }
-  else if (class(points)[1] == "SpatialPointsDataFrame") {
+  } else if (class(points)[1] == "SpatialPointsDataFrame") {
     names(points) <- c("Station")
     if (crs_points != 4326)
       points <- sf::st_transform(sf::st_as_sf(points),
                                  4326)
-    points <- sf::as_Spatial(points)
     names(points) <- c("Station")
   }
+# hasta aqui
 
 
   if (class(atmos)[1] == "character") {
-    df <- list()
+ 
     if(verbose)   cat(paste0("Extracting data\n\n"))
-
-    for (i in 1:length(lr)) {
+ df <- list()
+lista <- list()
+for(j in 1:length(stations)) {
+	 for (i in 1:length(lr)) {
       df[[i]] <- raster::extract(x = lr[[i]],
-                                 y = points,
+                                 y = sf::as_Spatial(points[j,]),
                                  df = TRUE)
     }
     dft <- lapply(1:length(df), function(i) {
-      df[[i]]$ID <- NULL
+   df[[i]]$ID <- NULL
       names(df[[i]]) <- times
       dff <- data.frame(x = unlist(df[[i]]))
       dff
@@ -127,11 +128,15 @@ xtractor <- function (atmos,
     dft = do.call("cbind", dft)
     names(dft) <- c(vars)
     dft$Time <- times
+    dft$Station = stations[j]
+    dft <- sf::st_sf(dft, geometry = sf::st_sfc(st_as_sf(points)))
+    lista[[j]] <- dft
+	}
+    dft = do.call("rbind", lista)
 
-    dft$Station = rep(stations, each = length(times))
+    # times
     dft$Time <- as.POSIXct(x = dft$Time, format = "H%Y-%m-%d_%H:%M:%S",
                            tz = "GMT")
-
     dft$LT <- dft$Time
     attr(dft$LT, "tzone") <- tz
 
